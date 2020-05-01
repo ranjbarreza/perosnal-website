e0cf68e43423bcb95f1560fea289d14fb240c193
reza <reza_ranjbar94@yahoo.com>
2020-05-01 11:45:30 +0200

    updating color palette

--- personal-website/src/custom-styles/theme.js
+++ personal-website/src/custom-styles/theme.js
@@ -5,5 +5,13 @@ export const defaultTheme = {
   palette: {
     black: generateColor('#000000'),
     white: generateColor('#FFFFFF'),
+    darkGray: generateColor('#3d3d3d'),
+    mediumGray: generateColor('#848484'),
+    lightGray: generateColor('#bbbbbb'),
+    blueGray: generateColor('#A8B1B8'),
+    midnightBlue: generateColor('#00275E'),
+    mediumBLue: generateColor('#3D72A4'),
+    lightBlue: generateColor('#6D98AB'),
+    mySinYellowOrange: generateColor('#FEB729'),
   },
 };
